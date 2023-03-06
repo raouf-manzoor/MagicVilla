1b8b8cad9be2f08bec629732c798e2d43930a287
RaoufManzoor <raoufmanzoor@yahoo.com>
2023-03-07 02:08:04 +0500

    1. Added First API controller.

---
+++ MagicVilla_API/Controllers/VillaAPIController.cs
@@ -0,0 +1,20 @@
+﻿using MagicVilla_API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_API.Controllers
+{
+    [Route("api/VillaAPI")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Villa> GetVillas()
+        {
+
+            return new List<Villa>() {
+                new Villa() {Id = 1,Name="Pool View"},
+                new Villa() {Id = 2,Name="Beach View"}
+            };
+        }
+    }
+}
---
+++ MagicVilla_API/Models/Villa.cs
@@ -0,0 +1,8 @@
+﻿namespace MagicVilla_API.Models
+{
+    public class Villa
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
